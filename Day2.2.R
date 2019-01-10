84cab6656434e799cb8c540c7ac2c995de2f217d
aromatic-toast <34222068+aromatic-toast@users.noreply.github.com>
2019-01-09 23:19:24 -0800

    Update Day2.2.R

--- Day2.2.R
+++ Day2.2.R
@@ -1,3 +1,22 @@
+# Day 2: Part 2
+
+# The goal is to find the only two numbers in each row where one evenly divides the other - that is, 
+# where the result of the division operation is a whole number. They would like you to find those numbers on each line, 
+# divide them, and add up each line's result.
+
+# For example, given the following spreadsheet:
+
+5 9 2 8
+9 4 7 3
+3 8 6 5
+# In the first row, the only two numbers that evenly divide are 8 and 2; the result of this division is 4.
+# In the second row, the two numbers are 9 and 3; the result is 3.
+# In the third row, the result is 2.
+# In this example, the sum of the results would be 4 + 3 + 2 = 9.
+
+# What is the sum of each row's result in your puzzle input?
+
+
 #raw data file copied from website 
 
 raw.dat <- "4347	3350	196	162	233	4932	4419	3485	4509	4287	4433	4033	207	3682	2193	4223
